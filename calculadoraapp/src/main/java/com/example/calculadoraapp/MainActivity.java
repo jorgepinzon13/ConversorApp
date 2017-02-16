5dc2cc2812d97eee10741066471b18a938091b88
jorgepinzon13 <jorgepinzon13@gmail.com>
2017-02-15 23:32:07 -0500

    v2calc, una sola operacion

--- calculadoraapp/src/main/java/com/example/calculadoraapp/MainActivity.java
+++ calculadoraapp/src/main/java/com/example/calculadoraapp/MainActivity.java
@@ -8,12 +8,12 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText eResultado;
+    EditText eResultado,eResultadoPrevio;
     Button b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,bSum,bRes,bPor,bDiv,bPun,bC,bIgu;
 
-    String resultado,total;
+    String resultado,resultadoprevio,total,totalprevio;
 
-    Double acum;
+    Double acum,res;
 
     int x;
 
@@ -23,9 +23,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        eResultadoPrevio= (EditText) findViewById(R.id.eResultadoPrevio);
         eResultado= (EditText) findViewById(R.id.eResultado);
 
         total="";
+        totalprevio="";
+        resultadoprevio="";
 
         b0= (Button) findViewById(R.id.b0);
         b1= (Button) findViewById(R.id.b1);
@@ -58,58 +61,72 @@ public class MainActivity extends AppCompatActivity {
             case R.id.b0:
                 resultado= "0";
                 eResultado.setText(resultado.toString());
+                eResultadoPrevio.setText(resultado.toString());
+
                 break;
 
             case R.id.b1:
                 resultado= "1";
                 eResultado.setText(resultado.toString());
+                eResultadoPrevio.setText(resultado.toString());
                 break;
 
             case R.id.b2:
                 resultado= "2";
                 eResultado.setText(resultado.toString());
+                eResultadoPrevio.setText(resultado.toString());
                 break;
 
             case R.id.b3:
                 resultado= "3";
                 eResultado.setText(resultado.toString());
+                eResultadoPrevio.setText(resultado.toString());
                 break;
 
             case R.id.b4:
                 resultado= "4";
                 eResultado.setText(resultado.toString());
+                eResultadoPrevio.setText(resultado.toString());
                 break;
 
             case R.id.b5:
                 resultado= "5";
                 eResultado.setText(resultado.toString());
-            break;
+                eResultadoPrevio.setText(resultado.toString());
+                break;
 
             case R.id.b6:
                 resultado= "6";
                 eResultado.setText(resultado.toString());
-            break;
+                eResultadoPrevio.setText(resultado.toString());
+                break;
 
             case R.id.b7:
                 resultado= "7";
                 eResultado.setText(resultado.toString());
-            break;
+                eResultadoPrevio.setText(resultado.toString());
+                break;
 
             case R.id.b8:
                 resultado= "8";
                 eResultado.setText(resultado.toString());
-            break;
+                eResultadoPrevio.setText(resultado.toString());
+                break;
 
             case R.id.b9:
                 resultado= "9";
                 eResultado.setText(resultado.toString());
-            break;
+                eResultadoPrevio.setText(resultado.toString());
+                break;
 
 
             case R.id.bSum:
                 acum=Double.parseDouble(total);
                 x=1;
-                resultado= "+";
+                resultadoprevio=total+"+";
+                eResultadoPrevio.setText(resultadoprevio.toString());
+                resultado="";
+                total="";
                 eResultado.setText(resultado.toString());
 
                 break;
@@ -117,46 +134,88 @@ public class MainActivity extends AppCompatActivity {
             case R.id.bRes:
                 acum=Double.parseDouble(total);
                 x=2;
-                resultado= "-";
+                resultadoprevio=total+"-";
+                eResultadoPrevio.setText(resultadoprevio.toString());
+                resultado="";
+                total="";
                 eResultado.setText(resultado.toString());
                 break;
 
             case R.id.bPor:
                 acum=Double.parseDouble(total);
                 x=3;
-                resultado= "*";
+                resultadoprevio=total+"x";
+                eResultadoPrevio.setText(resultadoprevio.toString());
+                resultado="";
+                total="";
                 eResultado.setText(resultado.toString());
                 break;
 
             case R.id.bDiv:
                 acum=Double.parseDouble(total);
                 x=4;
-                resultado= "/";
+                resultadoprevio=total+"÷";
+                eResultadoPrevio.setText(resultadoprevio.toString());
+                resultado="";
+                total="";
                 eResultado.setText(resultado.toString());
                 break;
 
             case R.id.bPun:
                 resultado= ".";
                 eResultado.setText(resultado.toString());
+                eResultadoPrevio.setText(resultado.toString());
                 break;
 
             case R.id.bC:
                 resultado= "";
                 total="";
+                resultadoprevio="";
                 eResultado.setText(resultado.toString());
+                eResultadoPrevio.setText(resultado.toString());
                 break;
 
             case R.id.bIgu:
-                acum=Double.parseDouble(total);
-                total="";
                 resultado="";
-                total=acum.toString();
+                resultadoprevio="";
+                switch (x){
+
+                    case 1:
+                        res=Double.parseDouble(total);
+                        res=acum+res;
+
+                        break;
+
+
+                    case 2:
+                        res=Double.parseDouble(total);
+                        res=acum-res;
+
+                        break;
+
+                    case 3:
+                        res=Double.parseDouble(total);
+                        res=acum*res;
+                        break;
+
+                    case 4:
+                        res=Double.parseDouble(total);
+                        res=acum/res;
+                        break;
+
+                }
+
+                total=res.toString();
                 break;
 
 
         }
 
+
+
         total=total+resultado;
+        totalprevio=resultadoprevio+total;
+        eResultadoPrevio.setText(totalprevio);
         eResultado.setText(total);
 
     }
