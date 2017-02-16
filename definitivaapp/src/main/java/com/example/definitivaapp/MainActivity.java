19008e0b1d46553dd6796401fcf2a463799aa794
jorgepinzon13 <jorgepinzon13@gmail.com>
2017-02-16 12:24:30 -0500

    v1 definitiva

--- definitivaapp/src/main/java/com/example/definitivaapp/MainActivity.java
+++ definitivaapp/src/main/java/com/example/definitivaapp/MainActivity.java
@@ -2,8 +2,17 @@ package com.example.definitivaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText ePractica, eAvance1, eAvance2, eAvance3, eAppfinal,eDef;
+
+    Button bCalcular;
+
+    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
